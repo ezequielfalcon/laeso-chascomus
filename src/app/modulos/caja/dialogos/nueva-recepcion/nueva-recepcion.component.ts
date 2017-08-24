2b230ec1bf7c4725a377f5969a1ec91ed4fc886b
Ezequiel Falcón <falconezequiel@gmail.com>
2017-08-24 17:15:17 -0300

    dialogo de recepcion con seleccionar proveedor

--- src/app/modulos/caja/dialogos/nueva-recepcion/nueva-recepcion.component.ts
+++ src/app/modulos/caja/dialogos/nueva-recepcion/nueva-recepcion.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewContainerRef} from '@angular/core';
 import {Proveedor} from '../../../../modelos/proveedor';
+import {ProveedoresService} from '../../../../servicios/datos/proveedores.service';
+import {SeleccionarProveedorService} from '../seleccionar-proveedor/seleccionar-proveedor.service';
+import {SpinnerService} from '../../../utils/directivas/spinner/spinner.service';
+import {NotificationsService} from 'angular2-notifications';
 
 @Component({
   selector: 'app-nueva-recepcion',
@@ -11,9 +15,35 @@ export class NuevaRecepcionComponent implements OnInit {
   proveedorSeleccionado = false;
   proveedor: Proveedor = new Proveedor;
 
-  constructor() { }
+  constructor(
+    private proveedoresService: ProveedoresService,
+    private selecProveedor: SeleccionarProveedorService,
+    private vcr: ViewContainerRef,
+    private spinner: SpinnerService,
+    private notificationsService: NotificationsService
+  ) { }
 
   ngOnInit() {
+  }
+
+  seleccionarProveedor() {
+    this.selecProveedor.seleccionarProveedor(this.vcr).subscribe(proveedorSeleccionadoDialogo => {
+      if (proveedorSeleccionadoDialogo) {
+        this.spinner.start();
+        this.cargarProveedor(proveedorSeleccionadoDialogo);
+      }
+    });
+  }
+
+  cargarProveedor(proveedorId: number) {
+    this.proveedoresService.verProveedor(proveedorId).subscribe(proveedorDb => {
+      this.proveedor = proveedorDb;
+      this.proveedorSeleccionado = true;
+    }, error => {
+      const body = JSON.parse(error._body);
+      this.notificationsService.error('Error', body.mensaje);
+      this.spinner.stop();
+    });
   }
 
 }
