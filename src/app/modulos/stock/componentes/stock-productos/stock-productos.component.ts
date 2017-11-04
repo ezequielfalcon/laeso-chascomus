867a7507ade6fca36dfffaf51323687e01328f5f
Ezequiel Falcón <falconezequiel@gmail.com>
2017-11-04 11:42:58 -0300

    componente de productos con stock listo

--- src/app/modulos/stock/componentes/stock-productos/stock-productos.component.ts
+++ src/app/modulos/stock/componentes/stock-productos/stock-productos.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductoStock} from '../../../../modelos/producto-stock';
+import {NotificationsService} from 'angular2-notifications/dist';
+import {SpinnerService} from '../../../utils/directivas/spinner/spinner.service';
+import {StockService} from '../../../../servicios/datos/stock.service';
+import {Categoria} from '../../../../modelos/categoria';
+import {ProductosService} from '../../../../servicios/datos/productos.service';
 
 @Component({
   selector: 'app-stock-productos',
@@ -7,9 +13,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockProductosComponent implements OnInit {
 
-  constructor() { }
+  filtroNombre = '';
+  filtroCat = '';
+  filtroCodigo = '';
+  productosStock: ProductoStock[] = [];
+  categorias: Categoria[] = [];
+  columnas = [
+    { name: 'id', label: 'ID' },
+    { name: 'nombre', label: 'Nombre', sortable: true },
+    { name: 'codigo', label: 'Código', sortable: true },
+    { name: 'categoria', label: 'Categoría', sortable: true },
+    { name: 'stock_minimo', label: 'Stock mínimo', numeric: true, sortable: true },
+    { name: 'cantidad', label: 'Stock actual', numeric: true, sortable: true }
+  ];
+
+  constructor(
+    private stockService: StockService,
+    private spinner: SpinnerService,
+    private notificationsService: NotificationsService,
+    private productosService: ProductosService
+  ) { }
 
   ngOnInit() {
+    this.cargarCategorias();
+  }
+
+  cargarCategorias() {
+    this.productosService.verCategorias().subscribe(categoriasDb => {
+      this.categorias = categoriasDb;
+      this.cargarProductos();
+    }, error => {
+      const body = JSON.parse(error._body);
+      this.notificationsService.error('Error', body.mensaje);
+      this.spinner.stop();
+    });
+  }
+
+  cargarProductos() {
+    this.stockService.verProductosStock().subscribe(productosDb => {
+      this.productosStock = productosDb;
+      this.spinner.stop();
+    }, error => {
+      const body = JSON.parse(error._body);
+      this.notificationsService.error('Error', body.mensaje);
+      this.spinner.stop();
+    });
   }
 
 }
