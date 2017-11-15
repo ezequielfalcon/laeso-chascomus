632eae0d814527197b8988729fbda148efae65bf
Ezequiel Falcón <falconezequiel@gmail.com>
2017-11-15 11:12:16 -0300

    botón para abrir diálogo de ajuste desde stock por productos

--- src/app/modulos/stock/dialogos/nuevo-ajuste/nuevo-ajuste.service.ts
+++ src/app/modulos/stock/dialogos/nuevo-ajuste/nuevo-ajuste.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable, ViewContainerRef} from '@angular/core';
+import {NuevoAjusteComponent} from './nuevo-ajuste.component';
+import {MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material';
+import {Observable} from 'rxjs/Observable';
+import {ProductoStock} from '../../../../modelos/producto-stock';
 
 @Injectable()
 export class NuevoAjusteService {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
+
+  public crearAjuste(productos: ProductoStock[], viewContainerRef: ViewContainerRef): Observable<any> {
+    let dialogRef: MatDialogRef<NuevoAjusteComponent>;
+    const config = new MatDialogConfig();
+    config.viewContainerRef = viewContainerRef;
+    dialogRef = this.dialog.open(NuevoAjusteComponent, config);
+    dialogRef.componentInstance.productos = productos;
+    return dialogRef.afterClosed();
+  }
 
 }
