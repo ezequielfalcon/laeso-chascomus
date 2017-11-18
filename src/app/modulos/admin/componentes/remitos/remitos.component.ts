018cf46d410bd1546d79c1a5813904a59fc22c77
Ezequiel Falcón <falconezequiel@gmail.com>
2017-11-18 10:55:27 -0300

    componente de remitos para admin

---
+++ src/app/modulos/admin/componentes/remitos/remitos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remitos',
+  templateUrl: './remitos.component.html',
+  styleUrls: ['./remitos.component.css']
+})
+export class RemitosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
