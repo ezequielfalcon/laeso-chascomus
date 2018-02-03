f88902617128b2400976de847bb831acd880fc5f
Ezequiel Falcon <efalcon@hacienda-gcba.gov.ar>
2018-02-03 00:16:07 -0300

    botón de nuevo menú

--- src/app/modulos/stock/componentes/menus/menus.component.ts
+++ src/app/modulos/stock/componentes/menus/menus.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
-import { ProductosService } from '../../../../servicios/datos/productos.service';
 import { NotificationsService } from 'angular2-notifications';
 import { SpinnerService } from '../../../utils/directivas/spinner/spinner.service';
 import { Router } from '@angular/router';
 import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 import { Menu } from '../../../../modelos/menu';
+import { NuevoMenuService } from '../../dialogos/nuevo-menu/nuevo-menu.service';
+import { CocinaService } from '../../../../servicios/datos/cocina.service';
 
 @Component({
   selector: 'app-menus',
@@ -16,9 +17,10 @@ export class MenusComponent implements OnInit, OnDestroy {
   menus: Menu[] = [];
 
   constructor(
-    private productosService: ProductosService,
+    private cocinaService: CocinaService,
     private spinner: SpinnerService,
     private notificationsService: NotificationsService,
+    private nuevoMenuService: NuevoMenuService,
     private vcr: ViewContainerRef,
     public router: Router
   ) { }
@@ -31,8 +33,18 @@ export class MenusComponent implements OnInit, OnDestroy {
     this.spinner.start();
   }
 
+  nuevoMenu() {
+    this.nuevoMenuService.crearMenu(this.vcr).subscribe(
+      res => {
+        if (res) {
+          console.log('piola');
+        }
+      }
+    );
+  }
+
   cargarMenus() {
-    this.productosService.verMenus().subscribe(
+    this.cocinaService.verMenus().subscribe(
       menusDb => {
         this.menus = menusDb;
         console.log(menusDb);
